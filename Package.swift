2e3c48cb3375326d61eddde77acd953172f43266
Paul Leo <paul@tapdigital.com>
2023-11-02 14:42:06 +0000

    Custom Package added

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "TDGitGraphQL",
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v10_14),
+    .tvOS(.v12),
+    .watchOS(.v5),
+  ],
+  products: [
+    .library(name: "TDGitGraphQL", targets: ["TDGitGraphQL"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "TDGitGraphQL",
+      dependencies: [
+        .product(name: "ApolloAPI", package: "apollo-ios"),
+      ],
+      path: ".generated/schema/Sources"
+    ),
+  ]
+)
